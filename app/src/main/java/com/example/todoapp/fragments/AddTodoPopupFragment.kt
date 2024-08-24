5cad76cf2fef836a6a11b50d0d47f9cef24c1f81
Daria-Georgiana Feneșan <geo505georgiana@gmail.com>
2024-08-24 13:19:09 +0300

    Solved cursor color issue

--- app/src/main/java/com/example/todoapp/fragments/AddTodoPopupFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/AddTodoPopupFragment.kt
@@ -1,13 +1,11 @@
 package com.example.todoapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
-import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentAddTodoPopupBinding
 import com.example.todoapp.utils.ToDoData
 import com.google.android.material.textfield.TextInputEditText
@@ -89,6 +87,7 @@ class AddTodoPopupFragment : DialogFragment() {
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
     }
 
     interface DialogNextButtonClickListener{
